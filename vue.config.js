04e7e6939892094114cfa20125a7f487ae779133
Keisuke Onoue <k.onoue0724@gmail.com>
2022-06-17 09:18:01 +0900

    configured vue.ocnfig.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/mysite-vue/',
+  publicPath: './',
   outputDir: 'docs'
 })
