1b7998c3ae707abd5fc4a2bcec727dee2464831a
Keisuke Onoue <k.onoue0724@gmail.com>
2022-06-24 15:47:31 +0900

    trouble rendering a 3d object

--- src/components/contents/ContentsHero.vue
+++ src/components/contents/ContentsHero.vue
@@ -10,6 +10,16 @@ import * as THREE from "three";
 //import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { onMounted } from '@vue/runtime-core';
 
+
+// lifecycle hooks rendering test
+import { onBeforeMount } from '@vue/runtime-core';
+import { onBeforeUpdate } from '@vue/runtime-core';
+import { onUpdated } from '@vue/runtime-core';
+import { onBeforeUnmount } from '@vue/runtime-core';
+import { onUnmounted } from '@vue/runtime-core';
+
+
+
 export default {
   setup() {
     const scene = new THREE.Scene();
@@ -17,6 +27,31 @@ export default {
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material );
+
+
+
+    // lifecycle hooks rendering test 
+    onBeforeMount(() => {
+      console.log('onBeforeMount...');
+    });
+    onMounted(() => {
+      console.log('onMounted...');
+    });
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate...');
+    });
+    onUpdated(() => {
+      console.log('onUpdated...');
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount...');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted...');
+    });
+
+
+
 
     
     onMounted(() => {
